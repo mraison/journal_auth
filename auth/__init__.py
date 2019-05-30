df8c11acf49eaf1ecec8da9f88b23188b4318a30
raisonm <mattandthings@gmail.com>
2019-05-29 20:55:53 -0400

    hooking this server instance up to the proj database so that user data is accessible there. What I'll probably need to do at some point is construct a connection handler for these guys.

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,59 +1,20 @@
 from flask import Flask, abort
 # from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
-import base64
 from functools import wraps
 
 
 import os
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import sys
-import re
 from . import db
 
-import hashlib
-import hmac
-import json
 from .modules import jwt
 
 
 JWT_SECRET = 'secret'
-#######################################################
-# USER SET UP FOR TESTING.....
-#
-# @TODO replace this with a DB structure.
-class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __str__(self):
-        return "User(id='%s')" % self.id
-
-users = [
-    User(1, 'user1', 'abcxyz'),
-    User(2, 'user2', 'abcxyz'),
-]
-
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
-#######################################################
 
 #######################################################
-## utils for authenticating...
-def authenticate(username, password):
-    user = username_table.get(username, None)
-    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
-        return user
-
-def identity(payload):
-    user_id = payload['identity']
-    return userid_table.get(user_id, None)
-#######################################################
-
 def check_jwt(key):
     def decorator(f):
         @wraps(f)
@@ -86,7 +47,7 @@ def create_app(test_config=None):
         # a default secret that should be overridden by instance config
         SECRET_KEY='dev',
         # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, 'records.sqlite'),
+        DATABASE=os.path.join(app.instance_path, '../../journal_proj/instance/records.sqlite'),
     )
     # app.debug = True
     # app.config['SECRET_KEY'] = 'super-secret'
